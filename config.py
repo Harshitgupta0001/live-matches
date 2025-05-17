9f0fe0d97e2ec9a60dd73e2ced6e37e87361a711
Harshitgupta0001 <166941860+Harshitgupta0001@users.noreply.github.com>
2025-05-17 08:46:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -41,5 +41,5 @@ class Rkn_Bots(object):
     STICKER_ID = os.environ.get("STICKER_ID", "CAACAgIAAxkBAAELFqBllhB70i13m-woXeIWDXU6BD2j7wAC9gcAAkb7rAR7xdjVOS5ziTQE")
 
     # admin id  ( required.. 😥)
-    ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '6359874284 ').split()]
+    ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '6359874284 6186417426 ').split()]
     
