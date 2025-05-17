38ac36191b22bbe2f8c816fcabdee16a84349983
Harshitgupta0001 <166941860+Harshitgupta0001@users.noreply.github.com>
2025-05-17 15:55:17 +0530

    Update Reaction.py

--- Rkn_Bots/Reaction.py
+++ Rkn_Bots/Reaction.py
@@ -283,11 +283,11 @@ async def willow(client, message):
             # Format decryption keys
             keys = "\n".join([f"🔑 {key}" for key in match.get('playback_data', {}).get('keys', [])])
 
-            text = (f"<a href='{match['cover']}'>ㅤ</a> <b>{match['title']}</b>\n\n"
+            text = (f"<a href='{match['cover']}'>ㅤ</a> <b>{match['title']}</b>\n"
                     f"🏆 <b>Event Type:</b> {match.get('contentType', 'Cricket Match')}\n"
                     f"🕒 <b>Start Time:</b> {match['startTime']}\n"
-                    f"👥 <b>Teams:</b> {team1} vs {team2}\n\n"
-                    f"<b>Stream URLs:</b>\n{cdn_urls}\n\n"
+                    f"👥 <b>Teams:</b> {team1} vs {team2}\n"
+                    f"<blockquote expandable> Stream URLs:</b>\n{cdn_urls}\n</blockquote>"
                     f"<b>Decryption Keys:</b>\n{keys}\n\n"
                     f"<b>Note: Copy and paste the url in NS player or VLC media player in android and Autho iptv in pc to play stream</b>")
 
@@ -335,11 +335,11 @@ async def send_w_live_matches(client, chat_id):
         # Format decryption keys
         keys = "\n".join([f"🔑 {key}" for key in match.get('playback_data', {}).get('keys', [])])
 
-        text = (f" <a href='{match['cover']}'>ㅤ</a> <b>{match['title']}</b>\n\n"
+        text = (f" <a href='{match['cover']}'>ㅤ</a> <b>{match['title']}</b>\n"
                 f"🏆 <b>Event:</b> {match.get('contentType', 'Cricket Match')}\n"
                 f"🕒 <b>Start Time:</b> {match['startTime']}\n"
-                f"👥 <b>Teams:</b> {team1} vs {team2}\n\n"
-                f"<b>Stream URLs:</b>\n{cdn_urls}\n\n"
+                f"👥 <b>Teams:</b> {team1} vs {team2}\n"
+                f"<blockquote expandable><b>Stream URLs:</b>\n{cdn_urls}</blockquote>\n"
                 f"<b>Decryption Keys:</b>\n{keys}\n\n"
                 f"<b>Note: Copy and paste the url in NS player or VLC media player in android and Autho iptv in pc to play stream</b>")
 
@@ -361,7 +361,7 @@ async def auto_send_w_loop(client, chat_id):
         await send_w_live_matches(client, chat_id)
         await asyncio.sleep(1800)  # 30 minutes
 
-@Client.on_message(filters.command("wtv") & filters.user("Rkn_Bots.ADMIN"))
+@Client.on_message(filters.command("wtv") & filters.user(Rkn_Bots.ADMIN))
 async def wtv_handler(client, message):
     if len(message.command) < 2:
         return await message.reply("Usage:\n/willow on [channel_id]\n/willow off [channel_id]")
