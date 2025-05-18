04d1af0a2f65c4cfefaa8056227248c8cd6dce45
Harshitgupta0001 <166941860+Harshitgupta0001@users.noreply.github.com>
2025-05-18 13:28:07 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,13 +1,12 @@
 class script(object):
     START_TXT = """𝗛𝗲𝗹𝗹𝗼 {} 💞
-    ɪ ᴀᴍ Fancode Bot 🔥
+    <b>ɪ ᴀᴍ Live Sports Links Provider Bot 🔥</b>
+    I Extract Sports Urls From Drm Platforms Like <b>Willow, Sonylive, Fancode</b>
+    Click Help Button For More Information ✅
 <b><blockquote>Maintained By <a href='https://t.me/hgbotz'>𝙷𝙶𝙱𝙾𝚃ᶻ 🦋</a></blockquote></b>"""
 
     ABOUT_TXT = """<b><blockquote>⍟───[ MY ᴅᴇᴛᴀɪʟꜱ ]───⍟</blockquote>
-    
-‣ ᴍʏ ɴᴀᴍᴇ : <a href=https://t.me/Reaction_99bot>Prachi 💞</a>
-‣ ᴍʏ ʙᴇsᴛ ғʀɪᴇɴᴅ : <a href='tg://settings'>ᴛʜɪs ᴘᴇʀsᴏɴ</a> 
-‣ maintenar: <a href='https://t.me/hgbotz'>𝙷𝙶𝙱𝙾𝚃ᶻ 🦋</a> 
+‣ Maintenar: <a href='https://t.me/Harshit_contact_bot'>HaRsHiT 🦋</a> 
 ‣ ʟɪʙʀᴀʀʏ : <a href='https://docs.pyrogram.org/'>ᴘʏʀᴏɢʀᴀᴍ</a> 
 ‣ ʟᴀɴɢᴜᴀɢᴇ : <a href='https://www.python.org/download/releases/3.0/'>ᴘʏᴛʜᴏɴ 3</a> 
 ‣ ᴅᴀᴛᴀ ʙᴀsᴇ : <a href='https://www.mongodb.com/'>ᴍᴏɴɢᴏ ᴅʙ</a> 
@@ -15,5 +14,18 @@ class script(object):
 ‣ ʙᴜɪʟᴅ sᴛᴀᴛᴜs : ᴠ2.7.1 [sᴛᴀʙʟᴇ]></b>
 """
 
-    HELP_TXT = """<b>Fancode 🔥</b>
-<b><blockquote>Maintained By <a href='https://t.me/hgbotz'>𝙷𝙶𝙱𝙾𝚃ᶻ 🦋</a></blockquote></b>"""
+    HELP_TXT = """<b>🔥 Live Drms 🔥</b>
+<b><blockquote> Users Commands 🍿</b></blockquote>
+/willow <b>- For Willow Live Matches </b>
+<b>━━━━━━━✦✗✦━━━━━━━━━</b>
+/sonyliv - <b>For Sonyliv Live Matches</b>
+<b>━━━━━━━✦✗✦━━━━━━━━━</b>
+/fancode - <b>For Fancode Live Matches</b> 
+
+<b><blockquote>ADMINS ONLY</blockquote></b>
+/willowtv on [chat_id] - <b>For Loop Willow Auto Update Links In Any Channel </b>
+<b>━━━━━━━✦✗✦━━━━━━━━━</b>
+/fan on [chat_id] - <b>For Loop Fancode Auto Update Links In Any Channel</b>
+<b>━━━━━━━✦✗✦━━━━━━━━━</b>
+/sliv on [chat_id] - <b>For Loop Sonyliv Auto Update Links In Any Channel</b>
+<b><blockquote>Maintained By 💝 - <a href='https://t.me/Harshit_contact_bot'>HaRsHiT 🦋</a></blockquote></b>"""
