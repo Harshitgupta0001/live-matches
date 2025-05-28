d7a74c503560d3cba9faae422a32042f11e232fb
Harshitgupta0001 <166941860+Harshitgupta0001@users.noreply.github.com>
2025-05-28 14:00:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ from os import environ
 
 id_pattern = re.compile(r'^.\d+$')
 
-AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('AUTH_CHANNEL', '').split()] # give channel id with seperate space. Ex : ('-10073828 -102782829 -1007282828')
+AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('AUTH_CHANNEL', '-1002621450225').split()] # give channel id with seperate space. Ex : ('-10073828 -102782829 -1007282828')
 
 class Rkn_Bots(object):
     
